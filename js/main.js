d18410c8e77f87b84e7ebdec81f729ec9c239e83
croesym <matheuscroesy@gmail.com>
2022-09-28 09:29:33 -0300

    Alterações no tamanho máximo da descrição

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const url = "https://api.github.com/users/croesym/repos";
 const repos = document.querySelector('.projetos');
-const maxlength = 120;
 const reposmaxl = document.getElementById
 //consumir api do github em divs
 
@@ -8,10 +7,12 @@ fetch(url)
     .then(response => response.json())
     .then(data => {
         data.forEach(repo => {
+            const maxlength = 120;
+            const descriptionFormated = repo.description.substr(0, maxlength ) + '..';
             repos.innerHTML += `
             <div class="item" id="shadow">
                 <a href="${repo.html_url}" id="repo-tittle"><img src="assets/folder.svg" alt="">${repo.name}</a>
-                <p max>${repo.description}</p>
+                <p max>${descriptionFormated}</p>
                 <div class="align-icons">
                     <h2><img src="assets/star.svg" alt="">${repo.stargazers_count}</h2>
                     <h2><img src="assets/git-branch.svg" alt="">${repo.forks_count}</h2>
