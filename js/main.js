7f1e3d56c265bd4d92b1f2acdde49a8dad9ab4c2
Matheus Croesy <matheuscroesy@gmail.com>
2023-02-08 20:05:20 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 const repositories = document.querySelector(".projetos");
-<<<<<<< HEAD
-=======
-const reposmaxl = document.getElementById('#repos');
->>>>>>> a8a3c55e445ef7365392e5edf8334424a0745425
 
 function renderRepositories() {
   fetch(`https://api.github.com/users/croesym/repos`).then(async (res) => {
@@ -13,23 +9,16 @@ function renderRepositories() {
     let data = await res.json();
     data.map((item) => {
       let project = document.createElement("div");
-<<<<<<< HEAD
       let description = item.description || "";
       
       if (description.length > 120) {
         description = description.substring(0, 120) + "...";
       }
-=======
->>>>>>> a8a3c55e445ef7365392e5edf8334424a0745425
       
       project.innerHTML += `
             <div class="item" id="shadow">
                 <a href="${item.html_url}" id="repo-tittle"><img src="assets/folder.svg" alt="">${item.name}</a>
-<<<<<<< HEAD
                 <p max>${description}</p>
-=======
-                <p max>${item.description}</p>
->>>>>>> a8a3c55e445ef7365392e5edf8334424a0745425
                 <div class="align-icons">
                     <h2><img src="assets/star.svg" alt="">${item.stargazers_count}</h2>
                     <h2><img src="assets/git-branch.svg" alt="">${item.forks_count}</h2>
@@ -40,8 +29,4 @@ function renderRepositories() {
     });
   });
 }
-<<<<<<< HEAD
 renderRepositories();
-=======
-renderRepositories();
->>>>>>> a8a3c55e445ef7365392e5edf8334424a0745425
